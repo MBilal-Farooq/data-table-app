1cf6a7c6279dcc480e8c84ea516d8a6983bdb248
Bilal Farooq <mbilalfarooq95@gmail.com>
2022-04-23 14:16:30 +0500

    Basic Data Table setup

---
+++ src/dataTable/DataTable.tsx
@@ -0,0 +1,65 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/** Table Column Definition */
+export type ColumnType = {
+    id: string,
+    label: string, 
+    numeric: boolean,
+    width?: string
+}
+
+/** Table Props */
+export interface DataTableProps {
+    columns: Array<ColumnType>;
+    rows: Array<any>;
+    /** will be called on any row click */
+    onRowClick?: (rowData: any, rowIndex: number) => void;
+}
+
+/**
+ * Data Table Component
+ */
+export default function DataTable({rows, columns, onRowClick}: DataTableProps): JSX.Element {
+
+    /** Table Headers */
+    const tableHeader = () => {
+        return (
+            <thead>
+                <tr>
+                    {columns.map( (column) => {
+                        return (
+                            <th key={column.id}>{column.label}</th>
+                        );
+                    } )}
+                </tr>
+            </thead>
+        );
+    }
+
+    /** Table Body */
+    const tableBody = () => {
+        return (
+            <tbody>
+                {rows.map( (row, index) => {
+                    return (
+                        <tr key={`Row-${index}`} onClick={() => onRowClick?.(row, index)}>
+                            {columns.map( (column, colIndex) => {
+                            return (
+                                <td key={`Row-${index}-Col-${colIndex}`}>{row[column.id]}</td>
+                            )
+                        })}</tr>
+                    );
+                })}
+            </tbody>
+        )
+    }
+
+    return (
+        <div>
+            <table className="table table-hover">
+                {tableHeader()}
+                {tableBody()}
+            </table>
+        </div>
+    );
+}
